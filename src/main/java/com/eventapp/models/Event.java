637ef4b997a6da0094967dfd0ffc80b940bfe5af
Rafael Oliveira <rafaelbatistacg@gmail.com>
2020-01-08 11:11:12 -0300

    creating guest model and relating entities using @OneToMany and @ManyToOne

--- src/main/java/com/eventapp/models/Event.java
+++ src/main/java/com/eventapp/models/Event.java
@@ -1,11 +1,13 @@
 package com.eventapp.models;
 
 import java.io.Serializable;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Event implements Serializable{
@@ -21,6 +23,8 @@ public class Event implements Serializable{
 	private String date;
 	private String time;
 	
+	@OneToMany
+	private List<Guest> guests;
 	
 	public long getId() {
 		return id;
---
+++ src/main/java/com/eventapp/models/Guest.java
@@ -0,0 +1,46 @@
+package com.eventapp.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Guest {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private String cpf;
+	
+	@ManyToOne
+	private Event event;
+	
+	private String name;
+	private String cellphone;
+	
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCellphone() {
+		return cellphone;
+	}
+	public void setCellphone(String cellphone) {
+		this.cellphone = cellphone;
+	}
+	public Event getEvent() {
+		return event;
+	}
+	public void setEvent(Event event) {
+		this.event = event;
+	}
+	
+}
